fda5c6a309398870e158fd3b8b3f1f96f73b32af
Edwin de Ridder <edwin@blue-sun.nl>
2018-04-21 16:34:45 +0200

    Add migration to create table users

---
+++ database/migrations/2018_04_21_142755_create_table_users.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
+            $table->string('address');
+            $table->string('country');
+            $table->string('language');
+            $table->string('currency');
+            $table->string('phone')->nullable();
+            $table->decimal('balance', 10, 2)->default(0);
+            $table->timestamp('last_login')->nullable();
+            $table->tinyInteger('active')->nullable()->default(1);
+            $table->softDeletes();
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
