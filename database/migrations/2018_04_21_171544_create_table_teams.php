f24ebb1f374941ccc38cbbb193b02852db167ad5
Edwin de Ridder <edwin@blue-sun.nl>
2018-04-21 19:21:33 +0200

    Add migrations and model for table teams

---
+++ database/migrations/2018_04_21_171544_create_table_teams.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableTeams extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('team_name')->unique();
+            $table->string('city');
+            $table->string('rank');
+            $table->tinyInteger('active')->nullable()->default(1);
+            $table->softDeletes();
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('teams');
+    }
+}
