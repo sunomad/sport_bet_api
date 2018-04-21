e93ee6950982c9f9aefbd327066edff5d15a2718
Edwin de Ridder <edwin@blue-sun.nl>
2018-04-21 19:30:57 +0200

    Add migrations and model for table matches

---
+++ database/migrations/2018_04_21_171557_create_table_matches.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableMatches extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('home_team_id')->unsigned();
+            $table->integer('visiting_team_id')->unsigned();
+            $table->timestamp('match_date');
+            $table->tinyInteger('cancelled')->nullable()->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+            $table->foreign('home_team_id')->references('id')->on('teams');
+            $table->foreign('visiting_team_id')->references('id')->on('teams');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('matches');
+    }
+}
