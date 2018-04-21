2b7e43a9c91eb30df5f607656e0b82256d86edfb
Edwin de Ridder <edwin@blue-sun.nl>
2018-04-21 16:36:14 +0200

    Add model User

---
+++ app/Models/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['balance', 'active'];
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+//    protected $fillable = [
+//        'name', 'email', 'password',
+//    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+}
