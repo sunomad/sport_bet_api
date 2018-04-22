128ed4bd9ebcbb67febf6f596c9ed181206b694a
Edwin de Ridder <edwin@blue-sun.nl>
2018-04-22 16:25:47 +0200

    Add functionality to place bets

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -11,6 +11,34 @@ use App\Repositories\BookRepository;
 
 class BookController extends Controller
 {
+    /**
+     * Place a bet for a user on a match
+     */
+    public function placeBet(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'match_date'         => 'required|date',
+            'home_team_name'     => 'required',
+            'visiting_team_name' => 'required',
+            'bet_amount'         => 'required',
+            'predicted_winner'   => 'required',
+            'username'           => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            $result = ['success' => false, 'errors' => $validator->errors()->all()];
+            return response()->json($result, 422);
+        }
+        
+        $repository = new BookRepository();        
+        if(!$repository->saveBet($request->all())) {
+            Log::error('Unable to save bet');
+            $result = ['success' => false, 'errors' => ['Unable to save bet']];
+            return response()->json($result, 500);
+        }
+        
+        return response()->json(['success' => true], 200);
+    }
     
     /**
      * Get a book
@@ -33,7 +61,7 @@ class BookController extends Controller
         
         $repository = new BookRepository();
         $book = $repository->getBook($request->get('match_date'), $request->get('home_team_name'), $request->get('visiting_team_name'));
-                
+        
         return response()->json($book, 200);
     }
     
--- app/Repositories/BookRepository.php
+++ app/Repositories/BookRepository.php
@@ -2,6 +2,9 @@
 
 namespace App\Repositories;
 
+use App\Models\Bet;
+use App\Models\User;
+use App\Models\Team;
 use Illuminate\Support\Facades\DB;
 
 class BookRepository
@@ -29,6 +32,31 @@ class BookRepository
                 ->get();
         
         return $book;
+    }
+    
+    /**
+     * Save a bet
+     * 
+     * @param array $data
+     * @return boolean
+     */
+    public function saveBet($data)
+    {
+        $match = Db::table('matches')->select('matches.id')->where('match_date', $data['match_date'])
+                ->join('teams as th', 'matches.home_team_id', '=', 'th.id')
+                ->join('teams as tv', 'matches.visiting_team_id', '=', 'tv.id')
+                ->where('th.team_name', $data['home_team_name'])
+                ->where('tv.team_name', $data['visiting_team_name'])
+                ->first();
+        
+        $bet = new Bet();
+        $bet->match_id         = $match->id;
+        $bet->user_id          = User::where('username', $data['username'])->first()->id;
+        $bet->match_date       = $data['match_date'];
+        $bet->amount           = $data['bet_amount'];
+        $bet->predicted_winner = Team::where('team_name', $data['predicted_winner'])->first()->id;
+        
+        return $bet->save();
     }
 }
 
